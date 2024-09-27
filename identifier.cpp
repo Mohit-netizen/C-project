f265f82f9a45d5a42921870a7169fd9adccf38ba
Mohit44357 <mohitk44357@gmail.com>
2024-09-27 23:28:48 +0530

    <!-- created-->

---
+++ identifier.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+int main()
+{
+	//rule 01- identifier may contain alphabets,underscode and number
+	int ab_34=400;
+	std::cout<<ab_34<<"\n";
+	//Rule 02-identifier may start with underscore
+	// int _ab56=100;
+	// std::cout<<_ab56<<"\n";
+	//Rule 03 - identifier not start with number
+	/*int 56av_=67
+	std::count<<"value og 56av_="<<56av_;*/
+	//Rule 04-keyword are not use as an identifier 
+	/*int auto=345:
+	std::cout <<"value of auto="<<auto;*/
+	//Rule 05-identifier are casesensitive
+	// int A=34;
+	// std::cout<<"value of A="<<A;
+	//Rule 06- In identifier not use any special symbols other than underscore not even space
+	
+	
+	
+	
+	return 0;
+}
